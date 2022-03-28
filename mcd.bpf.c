88d4d4ffdbc4d4abf860c5a92deb29d90938d5c4
Giulio Sidoretti <giuliosidoretti@gmail.com>
2022-03-28 13:30:56 +0000

    ID and load in MCD stack done

--- mcd.bpf.c
+++ mcd.bpf.c
@@ -19,37 +19,48 @@
 #define EIP_TYPE_PT 0x22
 #define EIP_MCD_SIZE 40
 #define MAX_TTS_SHIFT 22
+#define MCD_STACK_LEN 12
+
+#define DELTA_KEY 0
+#define TTS_KEY 1
+#define ID_KEY 2
+#define LD_KEY 3
 
 struct tlv {
     __u8 type;
     __u8 len;
-};
+} __attribute__((packed));
 
 struct mcd {
-    __u16 id_ld;
+    __be16 id_ld;
     __u8 tts;
 } __attribute__((packed));
 
 /* key 0 is delta time, key 1 is tts template */
-bpf_map(eip_mcd_time, HASH, __u8, __u64, 2);
+bpf_map(eip_mcd_time, HASH, __u8, __u64, 4);
 
 HIKE_PROG(HIKE_PROG_NAME)
 {
-    struct mcd (*mcd_stack)[12];
+    struct mcd (*mcd_stack)[MCD_STACK_LEN];
     int hbh_remaining_bytes;
     int eip_remaining_bytes;
     struct ipv6_opt_hdr *hbh;
     struct hdr_cursor *cur;
     struct pkt_info *info;
     struct tlv *tlv_ptr;
-    __u8 key_delta = 0;
-    __u8 key_tts = 1;
+    __u8 key_delta = DELTA_KEY;
+    __u8 key_tts = TTS_KEY;
+    __u8 key_id = ID_KEY;
+    __u8 key_ld = LD_KEY;
     int hvm_ret = 0;
+    __u64 *template;
     int offset = 0;
     __u64 boottime;
     __u64 realtime;
-    __u64 *template;
     __u64 *delta;
+    __u64 *load;
+    __u16 id_ld;
+    __u64 *id;
     __u8 tts;
     int ret;
 
@@ -124,13 +135,15 @@ HIKE_PROG(HIKE_PROG_NAME)
     /* MCD stack */
     hike_pr_debug("EIP type: 0x%x", tlv_ptr->type);
     hike_pr_debug("TLV len: %u", tlv_ptr->len);
-    mcd_stack = (struct mcd (*)[12]) cur_header_pointer(ctx, cur, offset, sizeof(*mcd_stack));
+    mcd_stack = (struct mcd (*)[MCD_STACK_LEN]) cur_header_pointer(
+		    			 ctx, cur, offset, sizeof(*mcd_stack));
     if (unlikely(!mcd_stack))
         goto drop;
-    hike_pr_debug("mcd0 id_ld: %u, tts: %u", (*mcd_stack)[0].id_ld, (*mcd_stack)[0].tts);
+    hike_pr_debug("mcd0 id_ld: %u, tts: %u", (*mcd_stack)[0].id_ld,
+		    			     (*mcd_stack)[0].tts);
     /* shift right */
-    for (int i = 0; i < 11; ++i) {
-        (*mcd_stack)[11 - i] = (*mcd_stack)[10 - i];
+    for (int i = 0; i < MCD_STACK_LEN - 1; ++i) {
+        (*mcd_stack)[MCD_STACK_LEN-1-i] = (*mcd_stack)[MCD_STACK_LEN-2-i];
     }
     /* read boot time from kernel, read delta between kernel boot time
      * and user space clock real time from map. Add them up and get
@@ -147,7 +160,7 @@ HIKE_PROG(HIKE_PROG_NAME)
     }
     realtime = boottime + *delta;
     hike_pr_debug("real time (nanoseconds): 0x%llx", realtime);
-    /* get tts template from map */
+    /* get tts template from map and compute tts */
     template = bpf_map_lookup_elem(&eip_mcd_time, &key_tts);
     if (unlikely(!template)) {
         hike_pr_err("could not read tts template from map");
@@ -160,10 +173,25 @@ HIKE_PROG(HIKE_PROG_NAME)
     }
     tts = (__u8) (realtime >> *template);
     hike_pr_debug("tts : 0x%x", tts);
-    //TODO id_ld
-
+    /* read interface ID and load and build id_ld variable*/
+    id = bpf_map_lookup_elem(&eip_mcd_time, &key_id);
+    if (unlikely(!id)) {
+        hike_pr_err("could not read interface ID from map");
+            goto drop;
+    }
+    load = bpf_map_lookup_elem(&eip_mcd_time, &key_ld);
+    if (unlikely(!load)) {
+        hike_pr_err("could not read interface load from map");
+            goto drop;
+    }
+    /* first 12 bits represent the ID, last 4 bits represent the load */
+    id_ld = (*id & 0xfff) << 4 | ( *load & 0xf );
+    hike_pr_debug("id_ld: 0x%x", id_ld);
     /* add mcd to first position in stack */
-    *mcd_stack[0] = (struct mcd) { .id_ld = 0xff, .tts = tts };
+    *mcd_stack[0] = (struct mcd) {
+        .id_ld = bpf_htons(id_ld),
+        .tts = tts
+    };
     
 
 out:
